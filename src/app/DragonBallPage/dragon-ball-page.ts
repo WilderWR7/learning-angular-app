26f319ec62c2612a4bda3d4dceae1c0564185f32
Wilder A15 <wilder.mayta91@gmail.com>
2026-05-13 23:13:53 -0400

    feat: implement character addition functionality in Dragon Ball page

--- src/app/DragonBallPage/dragon-ball-page.ts
+++ src/app/DragonBallPage/dragon-ball-page.ts
@@ -12,9 +12,25 @@ interface Character {
   imports: [NgClass],
 })
 export class DragonBallPage {
+  name = signal('');
+  power = signal(0);
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9000 },
     { id: 2, name: 'Vegeta', power: 8500 },
     { id: 3, name: 'Gohan', power: 7000 },
+    { id: 4, name: 'Piccolo', power: 5000 },
   ]);
+  addCharacter() {
+    if (!this.name() || this.power() <= 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+    this.characters.update((chars) => [...chars, newCharacter]);
+    this.name.set('');
+    this.power.set(0);
+  }
 }
